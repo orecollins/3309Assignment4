98ff618a6fd21dfeac10069ff0e90c4b4749a59a
manashav <mvetrive@uwo.ca>
2021-12-03 14:28:18 -0500

    connected it to workbench

--- DBConnection.js
+++ DBConnection.js
@@ -3,11 +3,23 @@ const mysql = require('mysql');
 function newConnection()
 {
     let conn = mysql.createConnection({
-        host:'34.70.199.34',
+        host:'localhost',
         user: 'root',
-        password:'mypassword',
-        database:'MyStoreDB'
+        password:'your_current_password',
+        database:'amusement'
     });
     return conn;
 }
 module.exports = newConnection;
+
+// const mysql = require('mysql');
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'your_current_password',
+//   database: 'amusement'
+// });
+// connection.connect((err) => {
+//   if (err) throw err;
+//   console.log('Connected!');
+// });
--- initDB.js
+++ initDB.js
@@ -1,45 +1,15 @@
 const mysql = require('mysql');
 
 let conn = mysql.createConnection({
-    host:'34.134.96.244',
+    host:'localhost',
     user: 'root',
-    password: 'password',
-    database: 'MyStoreDB'
+    password:'your_current_password',
+    database:'amusement'
 });
 
 conn.connect();
 
-conn.query(`Drop Table Product`,
-                (err,rows,fields) => {
-                    if (err)
-                        console.log(err);
-                    else
-                        console.log('Table Dropped');
-                }
-            )
-conn.query(`CREATE TABLE Product
-            (
-                Description varchar(100),
-                Price   Dec(6,3),
-                imgPath varchar(100)
-            )
-            ` 
-            , (err,rows,fields) => {
-                if (err)
-                    console.log(err);
-                else
-                    console.log('Table Created');
-            })
-
-conn.query( `insert into Product values ("Table",200,"/imgs/Table.jpg")`
-            , (err,rows,fields) => {
-                if (err)
-                    console.log(err);
-                else
-                    console.log('One row inserted');
-            });
-
-conn.query( `select * from Product `
+conn.query( `SELECT fName FROM Customer WHERE email="1dd@gmail.com" `
             , (err,rows,fields) => {
                 if (err)
                     console.log(err);
@@ -48,5 +18,45 @@ conn.query( `select * from Product `
                 for (r of rows)
                     console.log(r);
             });
+
+// conn.query(`Drop Table Product`,
+//                 (err,rows,fields) => {
+//                     if (err)
+//                         console.log(err);
+//                     else
+//                         console.log('Table Dropped');
+//                 }
+//             )
+// conn.query(`CREATE TABLE Product
+//             (
+//                 Description varchar(100),
+//                 Price   Dec(6,3),
+//                 imgPath varchar(100)
+//             )
+//             ` 
+//             , (err,rows,fields) => {
+//                 if (err)
+//                     console.log(err);
+//                 else
+//                     console.log('Table Created');
+//             })
+
+// conn.query( `insert into Product values ("Table",200,"/imgs/Table.jpg")`
+//             , (err,rows,fields) => {
+//                 if (err)
+//                     console.log(err);
+//                 else
+//                     console.log('One row inserted');
+//             });
+
+// conn.query( `select * from Product `
+//             , (err,rows,fields) => {
+//                 if (err)
+//                     console.log(err);
+//                 else
+//                     console.log('One row inserted');
+//                 for (r of rows)
+//                     console.log(r);
+//             });
 
 conn.end();
